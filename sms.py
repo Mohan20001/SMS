35490a604fd8f3f18831626c619bdc799e0908ab
mohan putta <puttamohan2001@gmail.com>
2022-12-18 21:34:16 +0530

    SMS main file...

---
+++ sms.py
@@ -0,0 +1,32 @@
+import sys
+from twilio.rest import Client
+
+if(len(sys.argv) < 5 or len(sys.argv) > 5):
+    print( "[Info] " + sys.argv[0].split("\\")[-1] + " -n [Number] -m [msg]")
+    exit()
+
+num = 0
+txt = ""
+
+for i in range(1, 5):
+    if i%2 == 1:
+        match sys.argv[i]:
+            case "-n":
+                num = sys.argv[i+1]
+            case "-m":
+                txt = sys.argv[i+1]
+
+print()
+print("Mobile No: {}\nMsg: \'{}\'".format(num, txt))
+
+account_sid = "AC4fb64e1d6f3f331490bc6ec965ae9c20"
+auth_token = "9a797ac4f14c577554cc06039e9a6e14"
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+body=txt,
+from_="+12069446962",
+to=num
+)
+message.sid
+print("message sent successfully!")
